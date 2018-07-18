aac6c5b516bff6fc555e493fe2b938f96ba225b8
Minh Lee <minhlee.fre.mta@gmail.com>
2018-07-19 01:06:12 +0700

    refactor code, replace expression with method getPerimeter

--- src/Cylinder.java
+++ src/Cylinder.java
@@ -1,7 +1,7 @@
 public class Cylinder {
     public static double getVolume(int radius, int height){
         double baseArea = Math.PI * radius * radius;
-        double perimeter = 2 * Math.PI  * radius;
+        double perimeter = getPerimeter(radius);
         double volume = perimeter * height + 2 * baseArea;
         return volume;
     }
